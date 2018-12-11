3635c40786b53c5f926185187efd1b713fe218cf
Victor Zagorodny <post.vittorius@gmail.com>
2018-12-11 09:04:01 +0200

    docs: add script file with some sample code

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Your classes or methods go here:
+
+
+class Tool:
+    def my_method(self):
+        print("my_method called!")
+
+
+if __name__ == "__main__":
+    t = Tool()
+    t.my_method()
